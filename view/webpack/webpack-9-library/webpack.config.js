bc0dfa67750d60e0d28e1cbf9f9e57309543c22d
chentong <chentong@iqiguo.com>
2018-11-16 10:45:34 +0800

    fix

--- view/webpack/webpack-9-library/webpack.config.js
+++ view/webpack/webpack-9-library/webpack.config.js
@@ -6,7 +6,7 @@ module.exports =
         entry: './src/index.js',
         output: {
             path: path.resolve(__dirname, './dist'),
-            filename: 'webpack-numbers.js',
+            filename: 'morganchen-demo-webpack-number.js',
             //控制 library 如何以不同方式暴露的选项。 
             /**
              * 遍历：作为一个全局变量，通过 script 标签来访问（libraryTarget:'var'）。
@@ -14,11 +14,11 @@ module.exports =
              * window：通过 window 对象访问，在浏览器中（libraryTarget:'window'）。
              * UMD：在 AMD 或 CommonJS 的 require 之后可访问（libraryTarget:'umd'）。
              */
-            libraryTarget: 'umd',
-            globalObject: 'this',
-            // libraryExport: 'default',
+            libraryTarget: 'umd',// 定义打包方式Universal Module Definition,同时支持在CommonJS、AMD和全局变量使用
+            globalObject: 'this',// 定义全局变量,兼容node和浏览器运行，避免出现"window is not defined"的情况
+            libraryExport: 'default',// 对外暴露default属性，就可以直接调用default里的属性
             //暴露 library
-            library: 'webpackNumbers'
+            library: 'morganchenDemoWebpackNumber'// 指定类库名,主要用于直接引用的方式(比如使用script 标签)
         },
         externals: {
             //这意味着你的 library 需要一个名为 lodash 的依赖，这个依赖在用户的环境中必须存在且可用。
